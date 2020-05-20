ebbfc3ec0eb6db3ebd2c29c728dd6db22779bb62
yunjiNam <yj607252@gmail.com>
2020-05-20 20:43:20 +0900

    [Add] 프론트  초기 세팅 완료

--- src/Routes.js
+++ src/Routes.js
@@ -4,25 +4,21 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-// import Main_sr from './pages/SR/Main_sr/Main_sr';
-// import Login_sr from './pages/SR/Login_sr/Login_sr';
+import Main_sr from './pages/SR/Main_sr/Main_sr';
+import Login_sr from './pages/SR/Login_sr/Login_sr';
 
 import Main_yj from './pages/YJ/Main_yj/Main_yj';
 import Login_yj from './pages/YJ/Login_yj/Login_yj';
-
-
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          {/* <Route exact path="/main_sr" component={Main_sr} />
-          <Route exact path="/Login_sr" component={Login_sr} /> */}
+          <Route exact path="/main_sr" component={Main_sr} />
+          <Route exact path="/Login_sr" component={Login_sr} />
           <Route exact path="/main_yj" component={Main_yj} />
           <Route exact path="/" component={Login_yj} />
-          
-
         </Switch>
       </Router>
     )
